a6b30535472d9fce4d419dce969f57ead5c76d84
Michelle Mukami <you@example.com>
2019-07-21 20:48:05 +0300

    changes made in password_locker.py

--- password_locker.py
+++ password_locker.py
@@ -86,7 +86,7 @@ class Credential:
     @classmethod
     def find_by_site_name(cls, site_name):
         '''
-        Method that takes in a site_name and returns a credential that matches that site_name.
+        Method that takes in a site_name and returns a credentials.
         '''
         for credential in cls.credentials_list:
             if credential.site_name == site_name:
