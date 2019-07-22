9d5e32e34cb9245b0597f109863bb8ce78ff6667
Michelle Mukami <you@example.com>
2019-07-22 08:00:39 +0300

    changes made in run.py

--- run.py
+++ run.py
@@ -3,11 +3,11 @@
 import sys
 from password_locker import User,Credential
 
-def create_user(fname,lname,password):
+def create_user(firstname,lastname,password):
 	'''
 	Function to create a new user account
 	'''
-	new_user = User(fname,lname,password)
+	new_user = User(firstname,lastname,password)
 	return new_user
 
 def save_user(user):
@@ -19,14 +19,14 @@ def save_user(user):
 
 def verify_user(first_name,password):
 	'''
-	Function that verifies the existance of the user before creating credentials
+	Function that verifies the existence of the user before creating credentials
 	'''
 	checking_user = Credential.check_user(first_name,password)
 	return checking_user
 
 def generate_password():
 	'''
-	Function to generate a password automatically
+	Function to generate a password
 	'''
 	gen_pass = generate_password()
 	return gen_pass
@@ -58,7 +58,7 @@ def copy_credential(site_name):
 
 def main():
 	print(' ')
-	print('Hello! Welcome to Password Locker.')
+	print('Hello! Welcome to Password_Locker.')
 	while True:
 		print(' ')
 		print("-"*60)
@@ -68,9 +68,9 @@ def main():
 			break
 
 		elif short_code == 'ca':
-			print("-"*60)
+			print("-"*50)
 			print(' ')
-			print('To create a new account:')
+			print('Create a new account?:')
 			first_name = input('Enter your first name - ').strip()
 			last_name = input('Enter your last name - ').strip()
 			password = input('Enter your password - ').strip()
@@ -78,7 +78,7 @@ def main():
 			print(" ")
 			print(f'New Account Created for: {first_name} {last_name} using password: {password}')
 		elif short_code == 'li':
-			print("-"*60)
+			print("-"*50)
 			print(' ')
 			print('To login, enter your account details:')
 			user_name = input('Enter your first name - ').strip()
@@ -89,13 +89,13 @@ def main():
 				print(f'Welcome {user_name}. Please choose an option to continue.')
 				print(' ')
 				while True:
-					print("-"*60)
+					print("-"*50)
 					print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
 					short_code = input('Enter a choice: ').lower().strip()
-					print("-"*60)
+					print("-"*50)
 					if short_code == 'ex':
 						print(" ")
-						print(f'Goodbye {user_name}')
+						print(f'lovely day! {user_name}')
 						break
 					elif short_code == 'cc':
 						print(' ')
@@ -104,10 +104,10 @@ def main():
 						account_name = input('Enter your account\'s name - ').strip()
 						while True:
 							print(' ')
-							print("-"*60)
+							print("-"*50)
 							print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
 							psw_choice = input('Enter an option: ').lower().strip()
-							print("-"*60)
+							print("-"*50)
 							if psw_choice == 'ep':
 								print(" ")
 								password = input('Enter your password: ').strip()
